b49612b84b71bafa5324efaa9909ef77f69a94c4
Joe Kim <joekim6a@gmail.com>
2021-06-20 16:56:23 -0700

    Finish phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,19 @@
 const router = require('express').Router();
 
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
