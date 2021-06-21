c6d44d5cbcc7e4d635635b7cf854d9ba6158ce85
Joe Kim <joekim6a@gmail.com>
2021-06-20 18:52:01 -0700

    Log in validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
@@ -18,8 +20,19 @@ router.get( '/', restoreUser, (req, res) => {
     }
 });
 
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
 // Log in
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
@@ -45,4 +58,4 @@ router.delete('/',(_req, res) => {
     }
 );
 
-  module.exports = router;
+module.exports = router;
