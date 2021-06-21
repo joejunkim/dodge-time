4291597b20e37f115d60061bca63f5de8048d19e
Joe Kim <joekim6a@gmail.com>
2021-06-21 01:22:16 -0700

    Signup form added

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,21 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
@@ -46,6 +61,13 @@ const sessionReducer = (state = initialState, action) => {
     default:
       return state;
   }
+};
+
+export const restoreUser = () => async dispatch => {
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
 };
 
 export default sessionReducer;
