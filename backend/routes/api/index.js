10ae67359f55d09ef68f9eccc9527ed9ba3a2436
Joe Kim <joekim6a@gmail.com>
2021-06-20 18:41:20 -0700

    Phase 4 testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,21 +1,9 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-// GET /api/set-token-cookie
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      },
-    })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
+router.use('/session', sessionRouter);
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+router.use('/users', usersRouter);
 
 module.exports = router;
